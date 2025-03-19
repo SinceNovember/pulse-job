4333260836b0785322bb1a82ae9119d544852821
SinceNovember <83428190@qq.com>
2025-03-19 22:10:30 +0800

    feat: add transport.

--- pulse-job-transport/pulse-job-transport-netty/src/main/java/com/simple/pulsejob/transport/netty/NettyConnector.java
+++ pulse-job-transport/pulse-job-transport-netty/src/main/java/com/simple/pulsejob/transport/netty/NettyConnector.java
@@ -1,17 +1,21 @@
 package com.simple.pulsejob.transport.netty;
 
+import java.util.Collection;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ThreadFactory;
+
 import com.simple.pulsejob.common.JConstants;
 import com.simple.pulsejob.common.concurrent.JNamedThreadFactory;
 import com.simple.pulsejob.common.util.ClassUtil;
 import com.simple.pulsejob.common.util.Maps;
-import com.simple.pulsejob.transport.JConnection;
-import com.simple.pulsejob.transport.JConnectionManager;
-import com.simple.pulsejob.transport.JConnector;
-import com.simple.pulsejob.transport.UnresolvedAddress;
+import com.simple.pulsejob.common.util.Requires;
+import com.simple.pulsejob.transport.*;
 import com.simple.pulsejob.transport.channel.JChannelGroup;
+import com.simple.pulsejob.transport.netty.channel.NettyChannelGroup;
+import com.simple.pulsejob.transport.netty.estimator.JMessageSizeEstimator;
 import com.simple.pulsejob.transport.processor.ConsumerProcessor;
 import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.util.HashedWheelTimer;
 
@@ -48,6 +52,111 @@ public abstract class NettyConnector implements JConnector<JConnection> {
         this.nWorkers = nWorkers;
     }
 
+    protected void init() {
+        ThreadFactory workerFactory = workerThreadFactory();
+        worker = initEventLoopGroup(nWorkers, workerFactory);
 
+        bootstrap = new Bootstrap().group(worker);
+
+        JConfig child = config();
+        child.setOption(JOption.IO_RATIO, 100);
+
+        doInit();
+    }
+
+    protected abstract void doInit();
+
+
+    protected ThreadFactory workerThreadFactory() {
+        return new JNamedThreadFactory("connector", Thread.MAX_PRIORITY);
+    }
+
+    @Override
+    public Protocol protocol() {
+        return protocol;
+    }
+
+    @Override
+    public ConsumerProcessor processor() {
+        return processor;
+    }
+
+    @Override
+    public void withProcessor(ConsumerProcessor processor) {
+        setProcessor(this.processor = processor);
+    }
+
+    @Override
+    public JChannelGroup group(UnresolvedAddress address) {
+        Requires.requireNotNull(address, "address");
+
+        JChannelGroup group = addressGroups.get(address);
+        if (group == null) {
+            JChannelGroup newGroup = channelGroup(address);
+            group = addressGroups.putIfAbsent(address, newGroup);
+            if (group == null) {
+                group = newGroup;
+            }
+        }
+        return group;
+    }
+
+    @Override
+    public Collection<JChannelGroup> groups() {
+        return addressGroups.values();
+    }
+
+    public boolean addChannelGroup(Directory directory, JChannelGroup group) {
+
+    }
+
+    protected void setOptions() {
+        JConfig child = config();
+
+        setIoRatio(child.getOption(JOption.IO_RATIO));
+
+        bootstrap.option(ChannelOption.MESSAGE_SIZE_ESTIMATOR, JMessageSizeEstimator.DEFAULT);
+    }
+
+    /**
+     * A {@link Bootstrap} that makes it easy to bootstrap a {@link io.netty.channel.Channel} to use
+     * for clients.
+     */
+    protected Bootstrap bootstrap() {
+        return bootstrap;
+    }
+
+    /**
+     * Lock object with bootstrap.
+     */
+    protected Object bootstrapLock() {
+        return bootstrap;
+    }
+
+    protected EventLoopGroup worker() {
+        return worker;
+    }
+
+    protected JChannelGroup channelGroup(UnresolvedAddress address) {
+        return new NettyChannelGroup(address);
+    }
+
+    /**
+     * Sets consumer's processor.
+     */
+    @SuppressWarnings("unused")
+    protected void setProcessor(ConsumerProcessor processor) {
+        // the default implementation does nothing
+    }
+
+    /**
+     * Sets the percentage of the desired amount of time spent for I/O in the child event loops.
+     * The default value is {@code 50}, which means the event loop will try to spend the same
+     * amount of time for I/O as for non-I/O tasks.
+     */
+    public abstract void setIoRatio(int workerIoRatio);
+
+
+    protected abstract EventLoopGroup initEventLoopGroup(int nThreads, ThreadFactory tFactory);
 
 }
